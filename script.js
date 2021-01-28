0353d49c36a16156d0092c4838e0976c6ecdb244
Elle <bound_by_harmony@yahoo.com>
2021-01-28 18:11:58 +0000

    Form layout completed

---
+++ script.js
@@ -0,0 +1,31 @@
+// PLUG IN FOR CHIPS CLASS 
+$('.chips-placeholder').chips({
+    placeholder: 'Exclude',
+    secondaryPlaceholder: 'Exclude',
+  });
+             //METHOD
+//var instance = M.Chips.getInstance(elem);
+//https://materializecss.com/chips.html
+/* jQuery Method Calls
+      You can still use the old jQuery plugin method calls.
+      But you won't be able to access instance properties.
+
+      $('.chips').chips('methodName');
+      $('.chips').chips('methodName', paramName);
+*/
+
+
+// PLUG IN FOR FORM SELECTION
+$(document).ready(function(){
+    $('select').formSelect();
+  });
+            //METHOD
+//var instance = M.FormSelect.getInstance(elem);
+
+/* jQuery Method Calls
+    You can still use the old jQuery plugin method calls.
+    But you won't be able to access instance properties.
+
+    $('select').formSelect('methodName');
+    $('select').formSelect('methodName', paramName);
+*/
