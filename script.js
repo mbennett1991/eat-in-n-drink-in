28f0f86cebb42840a78e88868be9830a317ed347
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-01-30 13:45:42 +0000

    changes

--- script.js
+++ script.js
@@ -1,69 +1,88 @@
+$(document).ready(function () {
+// GLOBAL VARIABLES
+var search = "";
+var excluded = "gluten";
+var health1 = "&health=vegan";
+var health2 = "&health=peanut-free&health=tree-nut-free";
+var searchInput = $("#main");
+
+
 //START BUTTON TO NEXT PAGE
-function startPg(){
+function startPg() {
     $("#strtPage").addClass("hide");
     $("#userForm").removeClass("hide");
+    return false;
 };
+// $("strtBtn").on("click", function () {
+//     startPg();
+// })
 
 //
-$("#srchBtn").on("click", function(){
+$("#srchBtn").on("click", function () {
     $("#strtPage").addClass("hide");
     $("#userForm").addClass("hide");
     $("#ingredientList").removeClass("hide");
     $("#drinkBtn").removeClass("hide");
+    userInput();
+
 });
+
+function userInput() {
+    // Search input into value to go through API
+    search = searchInput.val().trim();
+    console.log(search);
+    // Call AJAX request
+    callAPIs(search);
+}
 
 // PLUG IN with jQuery
-$(document).ready(function(){
-  $('.sidenav').sidenav();
-});
+
+    $('.sidenav').sidenav();
 
 //PLUG IN FOR CAROUSEL 
-$(document).ready(function(){
+
     $('.carousel').carousel();
-});
 
 // PLUG IN FOR CHIPS CLASS 
 $('.chips-placeholder').chips({
     placeholder: 'Exclude',
     secondaryPlaceholder: 'Exclude',
-  });
-             //METHOD
+});
+//METHOD
 //var instance = M.Chips.getInstance(elem);
 //https://materializecss.com/chips.html
 /* jQuery Method Calls
       You can still use the old jQuery plugin method calls.
       But you won't be able to access instance properties.
-
+ 
       $('.chips').chips('methodName');
       $('.chips').chips('methodName', paramName);
 */
 
 
 // PLUG IN FOR FORM SELECTION
-$(document).ready(function(){
+
     $('select').formSelect();
-  });
-            //METHOD
+//METHOD
 //var instance = M.FormSelect.getInstance(elem);
 
 /* jQuery Method Calls
     You can still use the old jQuery plugin method calls.
     But you won't be able to access instance properties.
-
+ 
     $('select').formSelect('methodName');
     $('select').formSelect('methodName', paramName);
 */
 
 
 //PLUG IN FOR MODAL 
-$(document).ready(function(){
-    $('.modal').modal();        
-  });
-$("#over18").click(function(){
+    $('.modal').modal();
+
+$("#over18").click(function () {
     MyFunction();
     return false;
 });
-            //METHOD
+//METHOD
 //var instance = M.Modal.getInstance(elem);
 
 /* jQuery Method Calls
@@ -73,3 +92,53 @@ $("#over18").click(function(){
     $('.modal').modal('methodName');
     $('.modal').modal('methodName', paramName);
 */
+
+
+    
+
+    function callAPIs() {
+        // var favArr = [];
+        var apiID = "271242ec";
+        var apiKEY = "f01212876eaf371a9693f1c0ee6b6dc0";
+
+        var queryURL = "https://api.edamam.com/search?app_id=" + apiID + "&app_key=" + apiKEY + "&q=" + search + health1 + health2 + "&excluded=" + excluded;
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            console.log(response.hits[0].recipe.ingredientLines);
+
+
+            // var recipeList = $(".collapsible");
+            var listNum = 0;
+            // var recipeObj = {};
+            var headers = response.hits;
+            for (var i = 0; i < headers.length; i++) {
+                $("#header" + listNum).text(headers[i].recipe.label);
+                $("#recipe" + listNum).text(headers[i].recipe.ingredientLines);
+                $("#image" + listNum).attr("src", headers[i].recipe.image);
+                $("#link" + listNum).attr("href", headers[i].recipe.url);
+                listNum++;
+                console.log(headers[i].recipe.label)
+            }
+            // var storeHeader = headers[i].recipe.label;
+
+            $(".collapsible").collapsible();
+
+
+
+            $(".favourite").on("click", function (event) {
+                event.preventDefault();
+                $(this).text("favorite");
+                // favArr.push(storeHeader)
+                // console.log(favArr)
+
+            })
+        });
+
+    }
+    // localStorage.setItem("favArr", JSON.stringify(favArr));
+
+})
