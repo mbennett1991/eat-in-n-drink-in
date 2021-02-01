b7e8a6fe61b5d11b92cf820fb60d56d0d9addf1c
Elle <bound_by_harmony@yahoo.com>
2021-02-01 18:51:11 +0000

    Updated previous

--- script.js
+++ script.js
@@ -1,35 +1,26 @@
-// PLUG IN FOR CHIPS CLASS 
-$('.chips-placeholder').chips({
-    placeholder: 'Exclude',
-    secondaryPlaceholder: 'Exclude',
-  });
-             //METHOD
-//var instance = M.Chips.getInstance(elem);
-//https://materializecss.com/chips.html
-/* jQuery Method Calls
-      You can still use the old jQuery plugin method calls.
-      But you won't be able to access instance properties.
+//START BUTTON TO NEXT PAGE
+function startPg(){
+    $("#strtPage").addClass("hide");
+    $("#userForm").removeClass("hide");
+};
 
-      $('.chips').chips('methodName');
-      $('.chips').chips('methodName', paramName);
-*/
+//
+$("#srchBtn").on("click", function(){
+    $("#strtPage").addClass("hide");
+    $("#userForm").addClass("hide");
+    $("#ingredientList").removeClass("hide");
+    $("#drinkBtn").removeClass("hide");
+});
 
-
-// PLUG IN FOR FORM SELECTION
+// PLUG IN with jQuery
 $(document).ready(function(){
-    $('select').formSelect();
-  });
-            //METHOD
-//var instance = M.FormSelect.getInstance(elem);
+  $('.sidenav').sidenav();
+});
 
-/* jQuery Method Calls
-    You can still use the old jQuery plugin method calls.
-    But you won't be able to access instance properties.
-
-    $('select').formSelect('methodName');
-    $('select').formSelect('methodName', paramName);
-*/
-
+//PLUG IN FOR CAROUSEL 
+$(document).ready(function(){
+    $('.carousel').carousel();
+});
 
 //PLUG IN FOR MODAL 
 $(document).ready(function(){
